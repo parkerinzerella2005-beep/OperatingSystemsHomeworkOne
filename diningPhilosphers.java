2994806993aa05b92a4aa2fcf39b62c6c6ee6aea
parkerinzerella2005-beep <parker.inzerella2005@gmail.com>
2026-02-19 14:15:52 -0600

    changed the name of the file, added some vars, and made philosopher getter method (still need to implement input validation)

---
+++ diningPhilosphers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class diningPhilosphers {
+    public static void main(String[] args) {
+        int P;
+        int M;
+        int C;
+        
+    }
+    public int getPhilosophers(){
+        int P = 0;
+        System.out.println("How many Philoshopers/Chopsticks? (2-100)");
+        Scanner pScanner = new Scanner(System.in);
+        /*if(){
+            #this will be the input validatiion
+        }
+        */
+        return P;
+    }
+}
