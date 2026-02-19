198a23048237752d850fa829cfb77853082ede88
parkerinzerella2005-beep <parker.inzerella2005@gmail.com>
2026-02-19 14:56:12 -0600

    finished up the input validation should still be reviewed before merging with main

--- diningPhilosphers.java
+++ diningPhilosphers.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 import java.math.*;
+import java.util.InputMismatchException;
 
 public class diningPhilosphers {
     public static void main(String[] args) {
         int P;
         int M;
         int C;
-        
+        P = getPhilosophers();
     }
-    public int getPhilosophers(){
-        int P = 0;
-        System.out.println("How many Philoshopers/Chopsticks? (2-100)");
+    public static int getPhilosophers(){
         Scanner pScanner = new Scanner(System.in);
-        /*if(){
-            #this will be the input validatiion
+        int P = 0;
+        int nC = 0;
+        try {
+            System.out.print("How many Philoshopers/Chopsticks? (2-100): ");
+            while(nC == 0){
+                P = pScanner.nextInt();
+                if((P<100) && (P>2)){
+                    nC = 1;
+                }
+                else{
+                    System.out.print("Must enter number 2-100! Try again: ");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("You didnt enter a number please reattempt!");
         }
-        */
+        pScanner.close();
         return P;
     }
 }
