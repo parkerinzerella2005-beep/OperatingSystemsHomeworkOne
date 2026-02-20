c6141725b1ffa3245d8405c5afd5acf539990834
Parker <parker.inzerella2005@gmail.com>
2026-02-20 11:30:21 -0600

    switched over to one static scanner, and then I added some more error correction in main. I also added a dupilicate (similar) code for the meals for the philosophers

--- diningPhilosphers.java
+++ diningPhilosphers.java
@@ -1,22 +1,38 @@
 import java.util.Scanner;
 import java.math.*;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 
 public class diningPhilosphers {
+        static int P;
+        static int M;
+        static int C;
+
+        static Scanner myScanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        int P;
-        int M;
-        int C;
-        P = getPhilosophers();
+        
+
+        try {
+            P = getPhilosophers();
+        } catch (NoSuchElementException e){
+            System.out.print("Wrong input try again!");
+        }
+        try {
+            M = getMeals();
+        } catch (NoSuchElementException e){
+            System.out.print("Wrong input try again!");
+        }        
+       
     }
     public static int getPhilosophers(){
-        Scanner pScanner = new Scanner(System.in);
         int P = 0;
         int nC = 0;
         try {
             System.out.print("How many Philoshopers/Chopsticks? (2-100): ");
             while(nC == 0){
-                P = pScanner.nextInt();
+                P = myScanner.nextInt();
                 if((P<100) && (P>2)){
                     nC = 1;
                 }
@@ -27,7 +43,27 @@ public class diningPhilosphers {
         } catch (InputMismatchException e) {
             System.out.println("You didnt enter a number please reattempt!");
         }
-        pScanner.close();
         return P;
+    }
+    public static int getMeals(){
+
+        int M = 0;
+        int nC = 0;
+        try {
+            System.out.print("How many Meals? (2-100): ");
+            while(nC == 0){
+                M = myScanner.nextInt();
+                if((M<100) && (M>2)){
+                    nC = 1;
+                }
+                else{
+                    System.out.print("Must enter number 2-100! Try again: ");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("You didnt enter a number please reattempt!");
+        }
+
+        return M;
     }
 }
