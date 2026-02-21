60d5708fceb1b3fea36805ee282dcca2564fb61a
parkerinzerella2005-beep <parker.inzerella2005@gmail.com>
2026-02-20 18:12:12 -0600

    Error correction for Philosphers is solved and fully ready for test!

--- diningPhilospher.java
+++ diningPhilospher.java
@@ -2,30 +2,43 @@ import java.util.Scanner;
 import java.math.*;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
+import java.util.concurrent.Semaphore;
 
-public class diningPhilosphers {
+public class diningPhilospher implements Runnable {
         static int P;
         static int M;
-        static int C;
-
+        int id;
         static Scanner myScanner = new Scanner(System.in);
-
+     
 
     public static void main(String[] args) {
-        
-
         try {
             P = getPhilosophers();
         } catch (NoSuchElementException e){
             System.out.print("Wrong input try again!");
         }
+        if(P == 0){
+            return;
+        }
         try {
             M = getMeals();
         } catch (NoSuchElementException e){
             System.out.print("Wrong input try again!");
+        }
+        if(M == 0){
+            return;
         }        
        
     }
+
+    public diningPhilospher(int id){
+            this.id =id;
+        }
+    
+    public void run(){
+
+    }
+
     public static int getPhilosophers(){
         int P = 0;
         int nC = 0;
@@ -41,12 +54,14 @@ public class diningPhilosphers {
                 }
             }
         } catch (InputMismatchException e) {
-            System.out.println("You didnt enter a number please reattempt!");
+            myScanner.next();
+            P=0;
+            System.out.println("You didnt enter a number or your number was too big! Please reattempt!");
         }
         return P;
     }
+    
     public static int getMeals(){
-
         int M = 0;
         int nC = 0;
         try {
@@ -61,9 +76,10 @@ public class diningPhilosphers {
                 }
             }
         } catch (InputMismatchException e) {
+            myScanner.next();
+            M=0;
             System.out.println("You didnt enter a number please reattempt!");
         }
-
         return M;
     }
 }
