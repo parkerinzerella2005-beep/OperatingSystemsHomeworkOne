6c639c9995f3ab2b218009cbcec21a00c653fa35
Parker <parker.inzerella2005@gmail.com>
2026-02-22 06:58:35 -0600

    worked on timeing errors Meal ate is overlapping

--- diningPhilospher.java
+++ diningPhilospher.java
@@ -6,12 +6,26 @@ import java.util.concurrent.Semaphore;
 public class diningPhilospher implements Runnable {
     static int P;
     static int M;
-    static Semaphore[] Chopsticks;
+    static int totalM;
     static Thread[] diningPhilosphers;
+    static Semaphore[] Chopsticks;
     static Semaphore Meal;
+    //barrier things for entry 
+    static Semaphore mutex;
+    static Semaphore semaphoreHold;
+    static int threadCount;
+
+    //barrier things for entry 
+    static Semaphore exitMutex;
+    static Semaphore exitSemaphoreHold;
+    static int exitThreadCount;
+
     int id;
     static Scanner myScanner = new Scanner(System.in); 
     public static void main(String[] args) {
+        System.out.println("Program Arguments: -A 1");
+        System.out.println("Dining Philosophers Started");
+
         try {
             P = getPhilosophers();
         } catch (NoSuchElementException e){
@@ -20,8 +34,14 @@ public class diningPhilospher implements Runnable {
         if(P == 0){
             return;
         }
+        else if(P == 1){
+            System.out.println("PROGRAM TERMINATED");
+            System.out.println("Philosophers need 2 chopsticks to eat.");
+            return;
+        }
         try {
             M = getMeals();
+            totalM = M;
         } catch (NoSuchElementException e){
             System.out.print("Wrong input try again!");
         }
@@ -37,13 +57,27 @@ public class diningPhilospher implements Runnable {
 
         //setting Meal
         Meal = new Semaphore(1);
+
+        //setting up barrier so the philosphpers arive at the same time
+        mutex = new Semaphore(1);
+        semaphoreHold = new Semaphore(0);
+        threadCount = 0;
+
+        //setting up barrier so the philosophers all exit at the same time
+        exitMutex = new Semaphore(1);
+        exitSemaphoreHold = new Semaphore(0);
+        exitThreadCount = 0;
         
         //Initianilize array of Philosphers and start each one
         diningPhilosphers = new Thread[P];
         for(int i = 0; i<P; i++){
             diningPhilosphers[i] = new Thread(new diningPhilospher(i));
+            System.out.println("-Philosopher " + (i+1) + " starting.");
             diningPhilosphers[i].start();
         }
+    
+    
+        
     }   
 
     public diningPhilospher(int id){
@@ -63,27 +97,50 @@ public class diningPhilospher implements Runnable {
 
     public void run(){
         try{
+            mutex.acquire();
+            threadCount++;
+            if(threadCount == P){
+                System.out.println("--All Philosophers have arrived");
+                for(int i = 0; i<P;i++){
+                    semaphoreHold.release();
+                }
+                mutex.release();
+            }
+            else{
+                mutex.release();
+                semaphoreHold.acquire();
+            }
             //since the philosphers can eat more that one meal
             while(true){
             Meal.acquire();
             if(M>0){
                 //left chopstick
                 if(Chopsticks[id].tryAcquire()){
+                    System.out.println("---Philosopher " + (id+1) + "'s left chopstick IS available.");
                     //right chopstick
                     if(Chopsticks[(id+1) % P].tryAcquire()){
+                        System.out.println("---Philosopher " + (id+1) + "'s rigt chopstick IS available.");
+                        System.out.println("----Philosopher " + (id+1) + " grabs both chopsticks");
+                        System.out.println("----Philosopher " + (id+1) + " has a pair of chopsticks.");
+                        System.out.println("-----Philosopher " + (id+1) +  " is eating." + "\n");
                         M--;
                         Meal.release();
-
+                        System.out.println("Meals ate: " + (totalM-M));
                         //eating
                         int eating = 3 + (int) (Math.random() * 4);
                         for (int i = 0; i < eating; i++) {
                             Thread.yield();
                         }
                         
+                        
+                        //drops both chopsticks
+                        System.out.println("-------Philosopher " + (id+1) + " dropped his left chopstick.");
                         Chopsticks[id].release();
+                        System.out.println("-------Philosopher " + (id+1) + " dropped his right chopstick.");
                         Chopsticks[(id+1) % P].release();
 
                         //thinking
+                        System.out.println("--------Philosopher " + (id+1) + " is thinking.");
                         int thinking = 3 + (int) (Math.random() * 4);
                         for (int i = 0; i < thinking; i++) {
                             Thread.yield();
@@ -103,6 +160,20 @@ public class diningPhilospher implements Runnable {
             }
             else{
                 Meal.release();
+                exitMutex.acquire();
+                exitThreadCount++;
+                if(exitThreadCount == P){
+                    System.out.println("---------All Philosophers have finished eating.");
+                    for(int i = 0; i<P;i++){
+                        exitSemaphoreHold.release();
+                    }
+                    exitMutex.release();
+                }
+                else{
+                    exitMutex.release();
+                    exitSemaphoreHold.acquire();
+                }
+                System.out.println("----------Philosopher " + (id+1) + " has left the table.");
                 return;
             }
         }
@@ -115,10 +186,10 @@ public class diningPhilospher implements Runnable {
         int P = 0;
         int nC = 0;
         try {
-            System.out.print("How many Philoshopers/Chopsticks? (2-100): ");
+            System.out.print("How many philosophers should be created? (integer from 1-10000): ");
             while(nC == 0){
                 P = myScanner.nextInt();
-                if((P<100) && (P>2)){
+                if((P<=10000) && (P>=1)){
                     nC = 1;
                 }
                 else{
@@ -137,10 +208,10 @@ public class diningPhilospher implements Runnable {
         int M = 0;
         int nC = 0;
         try {
-            System.out.print("How many Meals? (2-100): ");
+            System.out.print("How many meals should the philosophers eat? (integer from 1-10000): ");
             while(nC == 0){
                 M = myScanner.nextInt();
-                if((M<100) && (M>2)){
+                if((M<=10000) && (M>=1)){
                     nC = 1;
                 }
                 else{
